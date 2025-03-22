907bd259532f7f4ef00f26241f872b8b2df4a353
Salbatti <stevegeorgetel@gmail.com>
2025-03-22 15:23:07 +0530

    Challenge 5(C++)

---
+++ challenge5.cpp
@@ -0,0 +1,12 @@
+class Solution {
+    public:
+        int getSum(int a, int b) {
+            while(b!=0){
+                int tmp=(a ^ b);
+                b=(a & b)<<1;
+                a= tmp;
+                
+            }
+            return a;
+        }
+    };
